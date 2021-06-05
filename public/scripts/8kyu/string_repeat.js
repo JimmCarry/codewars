c515d76b8136f796933a0b04015887893c1457d5
Jim Carry <zaceklukas.lz@gmail.com>
2021-06-05 23:00:04 +0200

    8kyu gravity_flip is done

---
+++ public/scripts/8kyu/string_repeat.js
@@ -0,0 +1,13 @@
+// Write a function called repeatStr which repeats the given string string exactly n times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+// 1. easily take string and with repeat we will iterate
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+}
+
+
+// solutions of other warriors from codewars what i chose for u and me :)
