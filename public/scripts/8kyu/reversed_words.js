cf3131d9668e4fd51066f053c5d4051b255e61db
Jim Carry <zaceklukas.lz@gmail.com>
2021-06-07 11:23:41 +0200

    few 8kyus r done

---
+++ public/scripts/8kyu/reversed_words.js
@@ -0,0 +1,12 @@
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Example:
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+
+function reverseWords(str){
+    return str.split(' ').reverse().join(' ');
+}
+
+// solutions of other warriors from codewars what i chose for u and me :)
---
+++ public/scripts/8kyu/sum_the_strings.js
@@ -0,0 +1,16 @@
+// Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
+
+// Example: (Input1, Input2 -->Output)
+
+// "4",  "5" --> "9"
+// "34", "5" --> "39"
+// Notes:
+
+// If either input is an empty string, consider it as zero.
+
+function sumStr(a,b) {
+    const sum = Number(a) + Number(b);
+    return sum.toString()
+}
+
+// solutions of other warriors from codewars what i chose for u and me :)
