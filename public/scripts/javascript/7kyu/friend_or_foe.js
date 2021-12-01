45dae57026b309a1bbdcbe3890c2136d0131abe2
Lukáš Žáček <zaceklukas.lz@gmail.com>
2021-12-01 10:00:32 +0100

    7kyu friend_or_foe is done

---
+++ public/scripts/javascript/7kyu/friend_or_foe.js
@@ -0,0 +1,13 @@
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// i.e.
+
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+
+const friend = f => f.filter(n => (isNaN(n) && n.length==4))
+
+// solutions of other warriors from codewars what i chose for u and me :)
