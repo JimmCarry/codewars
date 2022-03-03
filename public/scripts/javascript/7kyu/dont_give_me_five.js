8e41ead13e9d2920f36c47e47dafe0951b898754
Lukáš Žáček <zaceklukas.lz@gmail.com>
2022-03-03 12:24:16 +0100

    7kyu dont_give_me_five.js is done

---
+++ public/scripts/javascript/7kyu/dont_give_me_five.js
@@ -0,0 +1,58 @@
+// Don't give me five!
+// In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+// Examples:
+
+// 1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+// 4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+// The result may contain fives. ;-)
+// The start number will always be smaller than the end number. Both numbers can be also negative!
+
+// I'm very curious for your solutions and the way you solve it. Maybe someone of you will find an easy pure mathematics solution.
+
+// Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+// I have also created other katas. Take a look if you enjoyed this kata!
+
+let start = 1
+let end = 90
+
+function dontGiveMeFive(start, end)
+{
+    let newArray = [];
+    for (let i = start; i <= end; i++) {
+        if (i % 10 === 5 || i % 10 === -5) {
+            console.log(i)
+            continue;
+        } else if (i.toString().split('')[0] === '5' || i.toString().split('')[0] === '-5') {
+            continue
+        } else {
+            newArray.push(i);
+        }
+    };
+    return newArray.length;
+}
+
+dontGiveMeFive(start, end)
+
+// solutions of other warriors from codewars what i chose for u and me :)
+
+// function dontGiveMeFive(start, end) {
+//     let count = 0
+//     for (let i = start; i <= end; i++) {
+//       if (!/5/.test(i)) {
+//         count++
+//       }
+//     }
+//     return count
+//   }
+
+//   function dontGiveMeFive(start, end) {
+//     let count = 0;
+
+//     for (let i = start; i <= end; ++i)
+//         if (!i.toString().includes("5"))
+//             count++;
+
+//     return count;
+// }
